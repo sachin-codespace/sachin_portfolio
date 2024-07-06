f6e320428cc32548569ff66b428ce7fd3dc6b2f7
Sachin <sachinprajapatijaipur@gmail.com>
2024-07-06 12:11:55 +0530

    add more projects and make myworklist.jsx

---
+++ src/components/Mywork/MyWorkList.jsx
@@ -0,0 +1,32 @@
+import netflix from "../../assets/netflix.jpeg";
+import bubble_game from '../../assets/bubble_game.jpeg'
+import cash_calculator from '../../assets/cash_calculator.jpg'
+import spotify from '../../assets/spotify.jpeg'
+import stopwatch_timer from '../../assets/stopwatch_timer.jpeg'
+import vowel_counter from '../../assets/vowel_counter.jpeg'
+import age_calculator from '../../assets/age_calculator.jpeg'
+import weather from '../../assets/weather.jpeg'
+import calculator from '../../assets/calculator.jpeg'
+import todo_app from '../../assets/todo_app.jpeg'
+import coin_flip from '../../assets/coin_flip.jpeg'
+import fd_calculator from '../../assets/fd_calculator.jpeg'
+import key_codes from '../../assets/key_codes.jpeg'
+import temperature_converter from '../../assets/temperature_converter.jpeg'
+
+let projects = [{ title: "Netflix", imageSrc: netflix, webUrl: "https://sachin-codespace.github.io/netflix_home/" },
+{ title: "Bubble Game", imageSrc: bubble_game, webUrl: "https://sachin-codespace.github.io/bubble_game/" },
+{ title: "Spotify Clone", imageSrc: spotify, webUrl: "https://sachin-codespace.github.io/spotify/" },
+{ title: "Cash Calculator", imageSrc: cash_calculator, webUrl: "https://sachin-codespace.github.io/cash_calculator/" },
+{ title: "StopWatch Timer", imageSrc: stopwatch_timer, webUrl: "https://sachin-codespace.github.io/stopwatch/" },
+{ title: "Vowel Counter", imageSrc: vowel_counter, webUrl: "https://sachin-codespace.github.io/vowel_counter/" },
+{ title: "Temperature Converter", imageSrc: temperature_converter, webUrl: "https://sachin-codespace.github.io/temperature-convert/" },
+{ title: "FD Calculator", imageSrc: fd_calculator, webUrl: "https://sachin-codespace.github.io/fd_calculator/" },
+{ title: "Age Calculator", imageSrc: age_calculator, webUrl: "https://sachin-codespace.github.io/age_calculator/" },
+{ title: "Coin Flip ", imageSrc: coin_flip, webUrl: "https://sachin-codespace.github.io/coin-flip/" },
+{ title: "Key Codes", imageSrc: key_codes, webUrl: "https://sachin-codespace.github.io/key-codes/" },
+{ title: "Weather", imageSrc: weather, webUrl: "https://sachin-codespace.github.io/weather_app/" },
+{ title: "ToDo App", imageSrc: todo_app, webUrl: "https://sachin-codespace.github.io/todo_app/" },
+{ title: "Calculator", imageSrc: calculator, webUrl: "https://sachin-codespace.github.io/calculator/" }
+]
+
+export default projects;
--- src/components/Mywork/Mywork.jsx
+++ src/components/Mywork/Mywork.jsx
@@ -1,45 +1,22 @@
 import React from "react";
 import './Mywork.css';
-import netflix from "../../assets/netflix.jpeg";
-import bubble_game from '../../assets/bubble_game.jpeg'
-import cash_calculator from '../../assets/cash_calculator.jpg'
-import spotify from '../../assets/spotify.jpeg'
-import stopwatch_timer from '../../assets/stopwatch_timer.jpeg'
-import vowel_counter from '../../assets/vowel_counter.jpeg'
-import age_calculator from '../../assets/age_calculator.jpeg'
-import weather from '../../assets/weather.jpeg'
-import calculator from '../../assets/calculator.jpg'
-import todo_app from '../../assets/todo_app.jpeg'
-
-
+import projects from "./MyWorkList";
 
 const Mywork = () => {
-  let projects = [{ title: "Netflix", imageSrc: netflix, webUrl: "https://sachin-codespace.github.io/netflix_home/" },
-  { title: "Bubble Game", imageSrc: bubble_game, webUrl: "https://sachin-codespace.github.io/bubble_game/" },
-  
-  { title: "Spotify Clone", imageSrc: spotify, webUrl: "https://sachin-codespace.github.io/spotify/" },
-  { title: "Cash Calculator", imageSrc: cash_calculator, webUrl: "https://sachin-codespace.github.io/cash_calculator/" },
-  { title: "StopWatch Timer", imageSrc: stopwatch_timer, webUrl: "https://sachin-codespace.github.io/stopwatch/" },
-  { title: "Vowel Counter", imageSrc: vowel_counter, webUrl: "https://sachin-codespace.github.io/vowel_counter/" },
-  { title: "Age Calculator", imageSrc: age_calculator, webUrl: "https://sachin-codespace.github.io/age_calculator/" },
-  { title: "Weather", imageSrc: weather, webUrl: "https://sachin-codespace.github.io/weather_app/" },{ title: "ToDo App", imageSrc: todo_app, webUrl: "https://sachin-codespace.github.io/todo_app/" },
-  { title: "Calculator", imageSrc: calculator, webUrl: "https://sachin-codespace.github.io/calculator/" },
-  ]
-
   return (
     <div id="work" className="mywork">
       <h1 className="mywork-heading">My Latest Work</h1>
       <div className="works">
         {projects.map((project) => {
           return (
-            <a key={project.title} target="_blank" href={project.webUrl}> <img className="work" title={project.title} src={project.imageSrc} />
+            <a key={project.title} target="_blank" rel="noopener noreferrer" href={project.webUrl}>
+              <img className="work" title={project.title} src={project.imageSrc} alt={project.title} />
             </a>
-
           )
         })}
-
       </div>
     </div>
   )
 }
-export default Mywork
+
+export default Mywork;
