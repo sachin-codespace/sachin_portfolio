ba98cf1c169f39c64fc3b799b91052c8dae2481c
Sachin <sachinprajapatijaipur@gmail.com>
2024-07-06 10:55:42 +0530

    projects changes

--- src/components/Mywork/Mywork.jsx
+++ src/components/Mywork/Mywork.jsx
@@ -1,33 +1,45 @@
 import React from "react";
 import './Mywork.css';
-import one from "../../assets/one.jpeg";
-import two from '../../assets/two.jpeg'
-import three from '../../assets/three.jpeg'
-import four from '../../assets/four.jpeg'
-import five from '../../assets/five.jpeg'
-import six from '../../assets/six.jpg'
-// import seven from '../../assets/seven.jpeg'
-const Mywork = () => {
-    return (
-        <div id="work" className="mywork">
-            <h1 className="mywork-heading">My Latest Work</h1>
-            <div className="works">
+import netflix from "../../assets/netflix.jpeg";
+import bubble_game from '../../assets/bubble_game.jpeg'
+import todo_app from '../../assets/todo_app.jpeg'
+import spotify from '../../assets/spotify.jpeg'
+import weather from '../../assets/weather.jpeg'
+import calculator from '../../assets/calculator.jpg'
 
-                <a target="_blank" href="https://sachin-codespace.github.io/netflix_home/"> <img className="work" src={one} />
-                </a>           
-                  <a target="_blank" href="https://sachin-codespace.github.io/bubble_game/">  <img className="work" src={two} />
-                </a>      
-                
-                   
-                  <a target="_blank" href="https://sachin-codespace.github.io/todo_app/">   <img className="work" src={three} /></a>
-                <a target="_blank" href="https://sachin-codespace.github.io/spotify/">   <img className="work" src={four} />
-                </a>           
-                  <a target="_blank" href="https://sachin-codespace.github.io/weather_app/">   <img className="work" src={five} />
-                </a>           
-                  <a target="_blank" href="https://sachin-codespace.github.io/calculator/">   <img className="work" src={six} />
-                </a>             
-            </div>
-        </div>
-    )
+import age_calculator from '../../assets/calculator.jpg'
+import cash_calculator from '../../assets/cash_calculator.jpg'
+import stopwatch_timer from '../../assets/stopwatch_timer.jpeg'
+import vowel_counter from '../../assets/vowel_counter.jpeg'
+
+
+const Mywork = () => {
+  let projects = [{ title: "Netflix", imageSrc: netflix, webUrl: "https://sachin-codespace.github.io/netflix_home/" },
+  { title: "Bubble Game", imageSrc: bubble_game, webUrl: "https://sachin-codespace.github.io/bubble_game/" },
+  { title: "ToDo App", imageSrc: todo_app, webUrl: "https://sachin-codespace.github.io/todo_app/" },
+  { title: "Spotify Clone", imageSrc: spotify, webUrl: "https://sachin-codespace.github.io/spotify/" },
+  { title: "Weather", imageSrc: weather, webUrl: "https://sachin-codespace.github.io/weather_app/" },
+  { title: "Calculator", imageSrc: calculator, webUrl: "https://sachin-codespace.github.io/calculator/" },
+  { title: "age_calculator", imageSrc: age_calculator, webUrl: "https://sachin-codespace.github.io/age_calculator/" },
+  { title: "Cash Calculator", imageSrc: cash_calculator, webUrl: "https://sachin-codespace.github.io/cash_calculator/" },
+  { title: "StopWatch Timer", imageSrc: stopwatch_timer, webUrl: "https://sachin-codespace.github.io/stopwatch/" },
+  { title: "Vowel Counter", imageSrc: vowel_counter, webUrl: "https://sachin-codespace.github.io/vowel_counter/" },
+  ]
+
+  return (
+    <div id="work" className="mywork">
+      <h1 className="mywork-heading">My Latest Work</h1>
+      <div className="works">
+        {projects.map((project) => {
+          return (
+            <a key={project.title} target="_blank" href={project.webUrl}> <img className="work" title={project.title} src={project.imageSrc} />
+            </a>
+
+          )
+        })}
+
+      </div>
+    </div>
+  )
 }
 export default Mywork
