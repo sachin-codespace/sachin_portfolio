615776577f3fdd0b46d8f01ecda1f983561e6be5
Sachin <sachinprajapatijaipur@gmail.com>
2024-11-16 12:56:47 +0530

    resume and projects added

--- src/components/Mywork/MyWorkList.jsx
+++ src/components/Mywork/MyWorkList.jsx
@@ -12,21 +12,29 @@ import coin_flip from '../../assets/coin_flip.jpeg'
 import fd_calculator from '../../assets/fd_calculator.jpeg'
 import key_codes from '../../assets/key_codes.jpeg'
 import temperature_converter from '../../assets/temperature_converter.jpeg'
-
+import travel from "../../assets/travel.jpg"
+import astro from "../../assets/astro.jpg"
+import digim from "../../assets/digim.jpg"
 let projects = [
+
+  { title: "Travel Agency", imageSrc: travel, webUrl: "https://travelsagencysite.netlify.app/" },
+  { title: "Digital Marketing Agency", imageSrc: digim, webUrl: "https://digim.netlify.app/" },
+  { title: "Astrology Services", imageSrc: astro, webUrl: "https://astrospheresite.netlify.app/" },
+
+
 { title: "Bubble Game", imageSrc: bubble_game, webUrl: "https://sachin-codespace.github.io/bubble_game/" },
 { title: "Spotify Clone", imageSrc: spotify, webUrl: "https://sachin-codespace.github.io/spotify/" },
 { title: "Cash Calculator", imageSrc: cash_calculator, webUrl: "https://sachin-codespace.github.io/cash_calculator/" },
-{ title: "StopWatch Timer", imageSrc: stopwatch_timer, webUrl: "https://sachin-codespace.github.io/stopwatch/" },
-{ title: "Vowel Counter", imageSrc: vowel_counter, webUrl: "https://sachin-codespace.github.io/vowel_counter/" },
+// { title: "StopWatch Timer", imageSrc: stopwatch_timer, webUrl: "https://sachin-codespace.github.io/stopwatch/" },
+// { title: "Vowel Counter", imageSrc: vowel_counter, webUrl: "https://sachin-codespace.github.io/vowel_counter/" },
 { title: "Temperature Converter", imageSrc: temperature_converter, webUrl: "https://sachin-codespace.github.io/temperature-convert/" },
 { title: "FD Calculator", imageSrc: fd_calculator, webUrl: "https://sachin-codespace.github.io/fd_calculator/" },
-{ title: "Age Calculator", imageSrc: age_calculator, webUrl: "https://sachin-codespace.github.io/age_calculator/" },
-{ title: "Coin Flip ", imageSrc: coin_flip, webUrl: "https://sachin-codespace.github.io/coin-flip/" },
-{ title: "Key Codes", imageSrc: key_codes, webUrl: "https://sachin-codespace.github.io/key-codes/" },
-{ title: "Weather", imageSrc: weather, webUrl: "https://sachin-codespace.github.io/weather_app/" },
-{ title: "ToDo App", imageSrc: todo_app, webUrl: "https://sachin-codespace.github.io/todo_app/" },
-{ title: "Calculator", imageSrc: calculator, webUrl: "https://sachin-codespace.github.io/calculator/" }
+// { title: "Age Calculator", imageSrc: age_calculator, webUrl: "https://sachin-codespace.github.io/age_calculator/" },
+// { title: "Coin Flip ", imageSrc: coin_flip, webUrl: "https://sachin-codespace.github.io/coin-flip/" },
+// { title: "Key Codes", imageSrc: key_codes, webUrl: "https://sachin-codespace.github.io/key-codes/" },
+// { title: "Weather", imageSrc: weather, webUrl: "https://sachin-codespace.github.io/weather_app/" },
+// { title: "ToDo App", imageSrc: todo_app, webUrl: "https://sachin-codespace.github.io/todo_app/" },
+// { title: "Calculator", imageSrc: calculator, webUrl: "https://sachin-codespace.github.io/calculator/" }
 ]
 
 export default projects;
